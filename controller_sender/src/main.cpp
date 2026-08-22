0ae6bc1f0d17f5394e074d2f50d706d63f280f64
imakuma-ryoichi <imakuma.ryoichi5957@mail.kyutech.jp>
2026-08-22 15:28:02 +0900

    error修正

--- controller_sender/src/main.cpp
+++ controller_sender/src/main.cpp
@@ -1,37 +1,50 @@
-#include "controller_input.hpp"
+#include <unistd.h>
+
+#include "controller.hpp"
 #include "wifi_sender.hpp"
 #include "bluetooth_sender.hpp"
 
-#include <unistd.h>
-#include <chrono>
-#include <iostream>
-#include <thread>
-
 int main()
 {
-    constexpr char DEVICE[] = "/dev/input/js0";
-    constexpr auto PERIOD = std::chrono::milliseconds(10);
+    const int controller_fd = open_controller();
 
-    const int controller_fd = open_controller(DEVICE);
-
-    if (controller_fd < 0) {
+    if (controller_fd < 0)
+    {
         return 1;
     }
 
-    ControllerData data{};
+    const int wifi_fd = create_wifi_sender("192.168.1.100", 12345);
 
-    WiFiSender wifi_sender{};
-    BluetoothSender bluetooth_sender{};
-
-    while (true) {
-        update_controller(controller_fd, data);
-
-        wifi_sender.send(data);
-        bluetooth_sender.send(data);
-
-        std::this_thread::sleep_for(PERIOD);
+    if (wifi_fd < 0)
+    {
+        close(controller_fd);
+        return 1;
     }
 
+    const int bluetooth_fd = connect_bluetooth("XX:XX:XX:XX:XX:XX", 1);
+
+    if (bluetooth_fd < 0)
+    {
+        close(wifi_fd);
+        close(controller_fd);
+        return 1;
+    }
+
+    while (true)
+    {
+        ControllerData data{};
+
+        if (!read_controller(controller_fd, data))
+        {
+            break;
+        }
+
+        send_wifi(wifi_fd, data);
+        send_bluetooth(bluetooth_fd, data);
+    }
+
+    close(bluetooth_fd);
+    close(wifi_fd);
     close(controller_fd);
 
     return 0;
